c1be131d86a918460030fadcc9b45fd9eccae728
spr9530 <145482692+spr9530@users.noreply.github.com>
2024-07-03 14:47:38 +0530

    Add files via upload

---
+++ src/components/MeetBox.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react'
+import tempV from '../assets/tempV.mp4'
+
+function MeetBox({videoRef}) {
+    useEffect(() => {
+        console.log('Video Ref:', videoRef); // Check if videoRef is correctly passed
+        if (videoRef && videoRef.current) {
+            console.log('Current Video Element:', videoRef.current); // Check current element reference
+        }
+    }, [videoRef]);
+    return (
+        <>
+            <div className='h-full w-full bg-black rounded-3xl text-white '>
+                <video className='h-full w-full object-cover rounded-3xl' ref={videoRef} autoPlay>
+                </video>
+            </div>
+        </>
+    )
+}
+
+export default MeetBox
